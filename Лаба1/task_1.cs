e89e352cc2c32bbec69142ba98c018103632b952
А Х <forgrace5@gmail.com>
2023-10-09 16:49:46 +0500

    Добавьте файлы проекта.

---
+++ Лаба1/task_1.cs
@@ -0,0 +1,169 @@
+﻿using System.Linq.Expressions;
+
+namespace Лаба1
+{
+    internal class task1
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вариант 10");
+            Console.WriteLine("---------------------------");
+            FirstTask();
+            Console.WriteLine("---------------------------");
+            Console.WriteLine($"Принадлежит ли точка области? Ответ: {SecondTask()}");
+            Console.WriteLine("---------------------------");
+            ThirdTask();
+        }
+
+        static void FirstTask()
+        {
+            double m, n;
+            bool isConvert;
+
+            Console.WriteLine("Задание 1. Вычисление значений");
+
+            do
+            {
+                Console.WriteLine("Привет! Введи число m");
+                isConvert = double.TryParse(Console.ReadLine(), out m);
+                if (!isConvert)
+                    Console.WriteLine($"Ошибка, вы ввели не число m. Повторите ввод");
+            } while (!isConvert);
+
+            do
+            {
+                Console.WriteLine("Привет! Введи число n");
+                isConvert = double.TryParse(Console.ReadLine(), out n);
+                if (!isConvert)
+                    Console.WriteLine("Ошибка, вы ввели не число n. Повторите ввод");
+            } while (!isConvert);
+
+            if (n != 1)
+            {
+                n -= 1;
+                double res1 = m / n++;
+                Console.WriteLine($"Значение выражения: (m / --n++) равно {res1}. Значение m={m}, n={n}");
+            }
+            else { Console.WriteLine("Невозможно вычислить (m / --n++), деление на 0 невозможно"); };
+
+            if (n != 0)
+            {
+                bool res2 = m / n < n--;
+                Console.WriteLine($"Значение выражения: (m / n < n--) равно {res2}. Значение m={m}, n={n}");
+            }
+            else { Console.WriteLine("Невозможно вычислить (m / n < n--), деление на 0 невозможно"); }
+
+            bool res3 = m + n++ > n + m;
+            Console.WriteLine($"Значение выражения: (m + n++ > n + m) равно {res3}. Значение m={m}, n={n}");
+
+            double x;
+
+            do
+            {
+                Console.WriteLine("Привет! Введи число x");
+                isConvert = double.TryParse(Console.ReadLine(), out x);
+                if (!isConvert)
+                    Console.WriteLine("Ошибка, вы ввели не число x. Повторите ввод");
+            } while (!isConvert);
+
+            double res4 = Math.Pow(x, 5) * Math.Sqrt(Math.Abs(x - 1)) + Math.Abs(25 - Math.Pow(x, 5));
+
+            Console.WriteLine($"Значение выражения: (x^5*sqrt(abs(x - 1)) + abs(25 - x^5)) равно {res4}");
+            
+        }
+
+
+        static bool SecondTask()
+        {
+            double x, y;
+            bool isConvert;
+
+            Console.WriteLine("Задание 2. Определение положения точки");
+
+            do
+            {
+                Console.WriteLine("Привет! Введи координату x");
+                isConvert = double.TryParse(Console.ReadLine(), out x);
+                if (!isConvert)
+                    Console.WriteLine("Введи координату x, вещественное число");
+            } while (!isConvert);
+
+            do
+            {
+                Console.WriteLine("Привет! Введи координату y");
+                isConvert = double.TryParse(Console.ReadLine(), out y);
+                if (!isConvert)
+                    Console.WriteLine("Введи координату y, вещественное число");
+            } while (!isConvert);
+
+            bool res;
+            
+            res = ((y >= -1 / 7.0 * x - 1) & (x <= 0) & (y <= 0));
+
+            return res;
+
+            /*if (res)
+            {
+                Console.WriteLine($"Точка ({x}, {y}) принадлежит заданной области.");
+            }
+            else { Console.WriteLine($"Точка ({x}, {y}) не лежит в заданной области."); };*/
+
+        }
+
+        static void ThirdTask()
+        {
+                       
+            Console.WriteLine("Задание 3. Вычисление примера с заданными данными");
+            double aDouble;
+            double bDouble;
+            aDouble = 1000;
+            bDouble = 0.0001;
+
+
+            double bufDouble1 = (double)Math.Pow(aDouble + bDouble, 3);
+
+            double bufDouble2 = (double)Math.Pow(aDouble, 3);
+
+            double bufDouble3 = (double)Math.Pow(aDouble, 2);
+
+            double bufDouble4 = (double)Math.Pow(bDouble, 2);
+
+            double bufDouble5 = (double)Math.Pow(bDouble, 3);
+
+            double bufDouble6 = (double)(bufDouble1 - (double)(bufDouble2 + (double)(3 * bufDouble3 * bDouble)));
+
+            double bufDouble7 = (double)((double)(3 * aDouble * bufDouble4) + bufDouble5);
+
+            double resDouble = bufDouble6 / bufDouble7;
+
+
+
+            float aFloat = 1000;
+            float bFloat = 0.0001f;
+
+            float bufFloat1 = (float)Math.Pow(aFloat + bFloat, 3);
+
+            float bufFloat2 = (float)Math.Pow(aFloat, 3);
+
+            float bufFloat3 = (float)Math.Pow(aFloat, 2);
+
+            float bufFloat4 = (float)Math.Pow(bFloat, 2);
+
+            float bufFloat5 = (float)Math.Pow(bFloat, 3);
+
+            float bufFloat6 = (float)(bufFloat1 - (float)(bufFloat2 + (float)(3 * bFloat * bufFloat3)));
+
+            float bufFloat7 = (float)((float)(3 * aFloat * bufFloat4) + bufFloat5);
+
+            float resFloat = bufFloat6 / bufFloat7;
+
+            Console.Write($"A - double, B - double. Ответ: ");
+            Console.WriteLine(resDouble);
+
+            Console.Write($"A - float, B - float. Ответ: ");
+            Console.WriteLine(resFloat);
+        }
+
+
+    }
+}
---
+++ Лаба2/tast 2.cs
@@ -0,0 +1,157 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+namespace Лаба2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Вариант 9. Задачи 1 - 9. Задача 2 - 26. Задача 3 - 43.");
+
+            Console.WriteLine("---------------------------");
+            FirstTask();
+            Console.WriteLine("---------------------------");
+            SecondTask();
+            Console.WriteLine("---------------------------");
+            ThirdTask();
+
+        }
+        
+        static void FirstTask()
+        {
+            int n;
+            bool isConvert;
+
+            Console.WriteLine("Задание 1. Дана последовательность из n целых чисел. Найти максимальный элемент в этой последовательности.");
+            Console.WriteLine("Привет! Введи длину последовательности.");
+            do
+            {
+                isConvert = int.TryParse(Console.ReadLine(), out n);
+                if (!isConvert)
+                {
+                    Console.WriteLine("Длина последовательности должна быть целым числом.");
+                    Console.WriteLine("Введи длину последовательности.");
+                }
+                else if (n < 0)
+                {
+                    Console.WriteLine("Длина последовательности должна быть положительной.");
+                    Console.WriteLine("Введи длину последовательности.");
+                    isConvert = false;
+                }
+                else if (n == 0)
+                {
+                    Console.WriteLine("Длина последовательности не может быть равна нулю.");
+                    Console.WriteLine("Введи длину последовательности.");
+                    isConvert = false;
+                };
+            } while (!isConvert);
+
+            int number, maxNumber, countMax;
+            maxNumber = -1000000000;
+            countMax = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введи целое число. Уже введено {i} чисел из {n}.");
+                do
+                {
+                    isConvert = int.TryParse(Console.ReadLine(), out number);
+                    if (!isConvert)
+                        Console.WriteLine("Ошибка. Нужно ввести целое число.");
+                } while (!isConvert);
+
+                if (maxNumber == number)
+                {
+                    countMax += 1;
+                }
+                else if (number > maxNumber)
+                {
+                    maxNumber = number;
+                    countMax = 1;
+                }
+                    
+
+            }
+
+            Console.WriteLine($"Максимальный элемент последовательности равен: {maxNumber}");
+            Console.WriteLine($"Количество максимальных элементов: {countMax}");
+
+        }
+
+        static void SecondTask()
+        {
+            Console.WriteLine("Задача 2. Дана последовательность целых чисел, за которой следует 0. Найти максимальный элемент в этой последовательности.");
+
+            bool isConvert, numberNotZero;
+            int number, maxNumber, countMax, index;
+
+            maxNumber = -1000000000;
+            countMax = 1;
+            index = 0;
+
+            do
+            {
+                Console.WriteLine("Введите число. Если хотите завершить работу - введите 0.");
+
+                do
+                {
+                    isConvert = int.TryParse(Console.ReadLine(), out number);
+                    if (!isConvert)
+                        Console.WriteLine("Ошибка. Нужно ввести целое число.");
+                } while (!isConvert);
+                                
+                if (number != 0)
+                {
+                    numberNotZero = true;
+                    // Количество введенных чисел
+                    index += 1;
+
+                    if (maxNumber == number)
+                    {
+                        countMax += 1;
+                    }
+                    else if (number > maxNumber)
+                    {
+                        maxNumber = number;
+                        countMax = 1;
+                    }
+                }
+                else
+                {
+                    numberNotZero = false;
+                }
+                                
+            } while (numberNotZero);
+
+
+            // Проверка длины последовательности
+            if (index == 0)
+            {
+                Console.WriteLine("Последовательность пуста.");
+            }
+            else
+            {
+                Console.WriteLine($"Максимальный элемент последовательности равен: {maxNumber}");
+                Console.WriteLine($"Количество максимальных элементов: {countMax}");
+            }
+        }
+
+        static void ThirdTask()
+        {
+            Console.WriteLine("Задание 3. Найти первое отрицательное число последовательности u=cos(ctg(n)), где n=1,2,3...");
+            
+            int n = 0;
+
+            do
+            {
+                n += 1;
+                Console.Write($"При n = {n} принимает значение: ");
+                Console.WriteLine(Math.Cos((float)1 / Math.Tan(n)));
+            } while (Math.Cos((float)1 / Math.Tan(n)) >= 0);
+            Console.WriteLine($"Уравнение u=cos(ctg(n)) впервые принимает отрицательное значение при n = {n}.");
+        }
+
+
+    }
+}
